4822bb05abdf528dd76ccc529832798e1cefa0db
msk1428 <msk1428@hotmail.com>
2019-01-16 16:59:19 +0300

    start project

---
+++ app/src/main/java/com/g7/mn/etmaen_g7/MainActivity.java
@@ -0,0 +1,17 @@
+package com.g7.mn.etmaen_g7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    public String TAG = "MainThis";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "this is running");
+    }
+}
