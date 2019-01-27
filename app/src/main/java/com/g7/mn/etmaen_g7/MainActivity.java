79945ad609a7d5a86298e35355e6c79b1d4be176
msk1428 <msk1428@hotmail.com>
2019-01-27 20:55:14 +0300

    main Activity +add+verify

--- app/src/main/java/com/g7/mn/etmaen_g7/MainActivity.java
+++ app/src/main/java/com/g7/mn/etmaen_g7/MainActivity.java
@@ -1,17 +1,39 @@
 package com.g7.mn.etmaen_g7;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    public String TAG = "MainThis";
+
+    @BindView(R.id.verify_person)
+    AppCompatButton verify_person;
+
+    @BindView(R.id.add_face)
+    AppCompatButton add_face;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "this is running");
+        ButterKnife.bind(this);
+
+
+     verify_person.setOnClickListener(view ->{
+         Intent intent = new Intent(this ,VerifyActivity.class);
+     });
+
+
+    add_face.setOnClickListener(view -> {
+        Intent intent = new Intent(this, AddFaceActivity.class);
+        startActivity(intent);
+    });
     }
 }
---
+++ app/src/main/java/com/g7/mn/etmaen_g7/VerifyActivity.java
@@ -0,0 +1,16 @@
+package com.g7.mn.etmaen_g7;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class VerifyActivity extends AppCompatActivity  {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.verify_face_layout);
+    }
+}
+
+
