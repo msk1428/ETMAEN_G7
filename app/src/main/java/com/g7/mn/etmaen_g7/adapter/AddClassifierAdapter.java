280ffdc6a3c9cd3c115276ed0d78b944dee34b41
xghida <42052554+xghida@users.noreply.github.com>
2019-02-19 19:53:59 +0300

    adapter file2

---
+++ app/src/main/java/com/g7/mn/etmaen_g7/adapter/AddClassifierAdapter.java
@@ -0,0 +1,119 @@
+package com.g7.mn.etmaen_g7.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.AdapterView;
+
+import com.bumptech.glide.Glide;
+import com.g7.mn.etmaen_g7.R;
+import com.g7.mn.etmaen_g7.database.AddEntry;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class AddClassifierAdapter extends RecyclerView.Adapter<AddClassifierAdapter.ClassifierViewHolder> {
+
+  final private  ItemClickListener mItemClickListener;  //3
+   private List <AddEntry> mImageEntries; // v. get all row in table
+    private Context mContext;
+
+    public AddClassifierAdapter(Context context, ItemClickListener listener){//4 consrtract object
+
+        mContext=context;
+        mItemClickListener=listener;
+    }
+//2 creat method
+    @NonNull
+    @Override
+    public ClassifierViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) { //holder call page of list item and display
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.add_classification_item, viewGroup, false);
+
+        return new ClassifierViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ClassifierViewHolder holder, int position) {
+        // Determine the values of the wanted data
+        AddEntry imageEntry = mImageEntries.get(position);
+        String name = imageEntry.getName();
+        String phonenumber = imageEntry.getPhonenumber();
+        String images = imageEntry.getImage();
+
+        //Set values
+        holder.name.setText(name);
+        holder.particulars.setText("" + phonenumber);
+
+        Glide.with(mContext)
+                .load(images)
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() { // get number of row of table
+        if (mImageEntries == null) {
+            return 0;
+        }
+        return mImageEntries.size();
+    }
+
+    public List<AddEntry> getClassifier() {
+        return mImageEntries;
+    } //get row from table
+
+    public void setTasks(List<AddEntry> imageEntries) { //set the v to table
+        mImageEntries = imageEntries;
+        notifyDataSetChanged();// notify the sys if have chane in DB such as add new row
+    }
+
+    public void removeItem(int position) { // if row deled it in DB the function know the adapter dosen't show
+        mImageEntries.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void restoreItem(AddEntry item, int position) { //recancell in DB
+        mImageEntries.add(position, item);
+        // notify item added by position
+        notifyItemInserted(position);
+    }
+
+    public interface ItemClickListener { // if press one of item open onther page
+        void onItemClickListener(int itemId);
+    }
+    // Inner class for creating ViewHolders  1- on click
+    public class ClassifierViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView name;
+        TextView particulars;
+        TextView status;
+        CircleImageView imageView;
+        RelativeLayout viewForeground;
+
+        ClassifierViewHolder(View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.name);
+            particulars = itemView.findViewById(R.id.particulars);
+            status = itemView.findViewById(R.id.status);
+            imageView = itemView.findViewById(R.id.image);
+            viewForeground = itemView.findViewById(R.id.view_foreground);
+            itemView.setOnClickListener(this);
+            imageView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int elementId = mImageEntries.get(getAdapterPosition()).getId();
+            mItemClickListener.onItemClickListener(elementId);
+        }
+    }
+}
+
---
+++ app/src/main/java/com/g7/mn/etmaen_g7/adapter/VerifyAdapter.java
@@ -0,0 +1,108 @@
+package com.g7.mn.etmaen_g7.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.g7.mn.etmaen_g7.R;
+import com.g7.mn.etmaen_g7.database.VerifiedEntry;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class VerifyAdapter extends RecyclerView.Adapter<VerifyAdapter.ClassifierViewHolder>{
+
+    // 3 Member variable to handle item clicks
+    final private ItemClickListener mItemClickListener;
+    private List<VerifiedEntry> mImageEntries;
+    private Context mContext;
+
+    public VerifyAdapter(Context context, ItemClickListener listener) {//4 obj
+        mContext = context;
+        mItemClickListener = listener;
+    }
+
+
+    @NonNull
+    @Override //5
+    public ClassifierViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {// call item page
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.verify_face_items, parent, false);
+
+        return new ClassifierViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ClassifierViewHolder holder, int position) {
+        // Determine the values of the wanted data
+        VerifiedEntry imageEntry = mImageEntries.get(position);
+        String name = imageEntry.getName();
+        String address = imageEntry.getAddress();
+        String phonenumber = imageEntry.getPhonenumber();
+        String images = imageEntry.getImage();
+
+        //Set values
+        holder.name.setText(name);
+        holder.phonenumber.setText(phonenumber);
+        holder.address.setText(address);
+
+        Glide.with(mContext)
+                .load(images)
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mImageEntries == null) {
+            return 0;
+        }
+        return mImageEntries.size();
+
+    }
+
+    public List<VerifiedEntry> getClassifier() {
+        return mImageEntries;
+    } //get
+    public void setTasks(List<VerifiedEntry> imageEntries) {//set
+        mImageEntries = imageEntries;
+        notifyDataSetChanged();
+    }
+
+    public interface ItemClickListener { //3
+        void onItemClickListener(int itemId);
+    }
+    // 1-Inner class for creating ViewHolders
+    public class ClassifierViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView name;
+        TextView phonenumber;
+        TextView address;
+        CircleImageView imageView;
+        public RelativeLayout viewForeground;
+
+        public ClassifierViewHolder(View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.name);
+            phonenumber = itemView.findViewById(R.id.phonenumber);
+            address = itemView.findViewById(R.id.address);
+            imageView = itemView.findViewById(R.id.image);
+            viewForeground = itemView.findViewById(R.id.view_foreground);
+            itemView.setOnClickListener(this);
+            imageView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int elementId = mImageEntries.get(getAdapterPosition()).getId();
+            mItemClickListener.onItemClickListener(elementId);
+        }
+    }
+}
